e6a6140eaf7d5bea5aaef3a39177d3c11394408d
Peter Mercer <peter@vizipix.com>
2018-09-25 18:45:02 +1000

    First build of Calendar tutorial added a component/Calendar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Calendar from './components/Calendar';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <div id="logo">
+                        <span className="icon">date_range</span>
+                        <span>
+              react<b>calendar</b>
+            </span>
+                    </div>
+                </header>
+                <main>
+                    <Calendar />
+                </main>
+            </div>
+        );
+    }
 }
 
 export default App;
