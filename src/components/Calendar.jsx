27c8f0a64ba66ff75c1cb62d03a66058c37f7412
Peter Mercer <peter@vizipix.com>
2018-09-26 17:50:03 +1000

    Final Calendar code Added Days and a seklectable day

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -44,9 +44,53 @@ class Calendar extends React.Component {
     }
 
     renderCells() {
+        const { currentMonth, selectedDate } = this.state;
+        const monthStart = dateFns.startOfMonth(currentMonth);
+        const monthEnd = dateFns.endOfMonth(monthStart);
+        const startDate = dateFns.startOfWeek(monthStart);
+        const endDate = dateFns.endOfWeek(monthEnd);
+
+        const dateFormat = "D";
+        const rows = [];
+
+        let days = [];
+        let day = startDate;
+        let formattedDate = "";
+
+        while (day <= endDate) {
+            for (let i = 0; i < 7; i++) {
+                formattedDate = dateFns.format(day, dateFormat);
+                const cloneDay = day;
+                days.push(
+                    <div
+                        className={`col cell ${
+                            !dateFns.isSameMonth(day, monthStart)
+                                ? "disabled"
+                                : dateFns.isSameDay(day, selectedDate) ? "selected" : ""
+                            }`}
+                        key={day}
+                        onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
+                    >
+                        <span className="number">{formattedDate}</span>
+                        <span className="bg">{formattedDate}</span>
+                    </div>
+                );
+                day = dateFns.addDays(day, 1);
+            }
+            rows.push(
+                <div className="row" key={day}>
+                    {days}
+                </div>
+            );
+            days = [];
+        }
+        return <div className="body">{rows}</div>;
     }
 
     onDateClick = day => {
+        this.setState(
+            {selectedDate:day}
+        );
     };
 
     nextMonth = () => {
