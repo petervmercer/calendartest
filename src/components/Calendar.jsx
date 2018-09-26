f7f2cd214fd12a036aae02b372c1e1b0f52663fc
Peter Mercer <Peter.Mercer@nps.org.au>
2018-09-26 17:03:50 +1000

    Added RenderDays code

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -29,11 +29,25 @@ class Calendar extends React.Component {
         );
     }
 
-    renderDays() {}
+    renderDays() {
+        const dateFormat = "dddd";
+        const days = [];
+        let startDate = dateFns.startOfWeek(this.state.currentMonth);
+        for (let i = 0; i < 7; i++) {
+            days.push(
+                <div className="col col-center" key={i}>
+                    {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+                </div>
+            );
+        }
+        return <div className="days row">{days}</div>;
+    }
 
-    renderCells() {}
+    renderCells() {
+    }
 
-    onDateClick = day => {};
+    onDateClick = day => {
+    };
 
     nextMonth = () => {
         this.setState({currentMonth: dateFns.addMonths(this.state.currentMonth, 1)});
