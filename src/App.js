52e8d98854d9facd095c2f60feeabcaa13a66580
Peter Mercer <peter@vizipix.com>
2018-09-28 08:32:38 +1000

    App.js converted to styled components for header added icon.css to test Google material designer

--- src/App.js
+++ src/App.js
@@ -1,30 +1,90 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Calendar from './components/Calendar';
-import './App.css';
+//import './App.css';
 import styled from "styled-components";
+import './styles/Theme/globalStyle';
 
 const AppWrapper = styled.div`
     text-align:center;
+`;
+
+const AppHeader = styled.div`
+    display: block;
+    width: 100%;
+    padding: 1.75em 0;
+    border-bottom: 1px solid #f9f9f9;
+    background: #fff;
+    padding-right: .25em;
+`;
+
+const AppLogo = styled.div`
+padding-right: .25em;
+`;
+
+const AppIcon = styled.span`
+
+@import url(https://fonts.googleapis.com/icon?family=Material+Icons);
+    padding-right: .25em;
+    font-family: 'Material Icons', serif;
+    font-style: normal;
+    display: inline-block;
+    vertical-align: middle;
+    line-height: 1;
+    text-transform: none;
+    letter-spacing: normal;
+    word-wrap: normal;
+    white-space: nowrap;
+    direction: ltr;
+
+    -webkit-font-smoothing: antialiased;
+    text-rendering: optimizeLegibility;
+    -moz-osx-font-smoothing: grayscale;
+    font-feature-settings: 'liga';
+     
+    cursor: pointer;
+    transition: .15s ease-out;
+    
+    &:hover {
+        transform: scale(1.75);
+        transition: .25s ease-out;
+        color: #1a8fff;
+    }
+    
+    &:first-of-type {
+        margin-left: 1em;
+    }
+    
+    &::last-of-type {
+        margin-right: 1em;
+    }
+    
 `;
 
 class App extends Component {
     render() {
         return (
             <AppWrapper>
-                <header>
-                    <div id="logo">
-                        <span className="icon">date_range</span>
+                <AppHeader>
+                    <AppLogo id="logo">
+                        <AppIcon >date_range</AppIcon>
                         <span>
-              react<b>calendar</b>
-            </span>
-                    </div>
-                </header>
-                <main>
-                    <Calendar />
-                </main>
+                            react<b>calendar</b>
+                        </span>
+                    </AppLogo>
+                </AppHeader>
+
             </AppWrapper>
         );
     }
 }
+
+/*
+</AppHeader>
+                <main>
+                    <Calendar/>
+                </main>
+
+
+ */
 
 export default App;
---
+++ src/styles/Theme/globalStyle.js
@@ -0,0 +1,21 @@
+import styled, {injectGlobal, css} from 'styled-components';
+
+injectGlobal`
+    @import url(https://fonts.googleapis.com/css?family=Open+Sans:300,400,700);
+    
+    
+    * {
+        box-sizing: border-box;
+    }
+
+body {
+    font-family: 'Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
+    font-size: 1em;
+    font-weight: 300;
+    line-height: 1.5;
+    color: #777;
+    background: #f9f9f9;
+    position: relative;
+}
+    
+    `;
