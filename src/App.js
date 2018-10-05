808625d7560025a2ed81bf4d1ba2ee4e5ab50e5c
Peter Mercer <Peter.Mercer@nps.org.au>
2018-10-05 17:32:52 +1000

    Changed main to Main and added Styled component syntax

--- src/App.js
+++ src/App.js
@@ -22,57 +22,18 @@ const AppLogo = styled.div`
 padding-right: .25em;
 `;
 
-const AppIcon = styled.span`
-
-@import url(https://fonts.googleapis.com/icon?family=Material+Icons);
-    padding-right: .25em;
-    font-family: 'Material Icons', serif;
-    font-style: normal;
-    display: inline-block;
-    vertical-align: middle;
-    line-height: 1;
-    text-transform: none;
-    letter-spacing: normal;
-    word-wrap: normal;
-    white-space: nowrap;
-    direction: ltr;
-
-    -webkit-font-smoothing: antialiased;
-    text-rendering: optimizeLegibility;
-    -moz-osx-font-smoothing: grayscale;
-    font-feature-settings: 'liga';
-    
-    font-size: 175%;
-    text-align: center;
-    color: #1a8fff;
-    line-height: 1;
-     
-    cursor: pointer;
-    transition: .15s ease-out;
-    
-    &:hover {
-        transform: scale(1.75);
-        transition: .25s ease-out;
-        color: #1a8fff;
-    }
-    
-    &:first-of-type {
-        margin-left: 1em;
-    }
-    
-    &::last-of-type {
-        margin-right: 1em;
-    }
-    
-    
-    
-`;
-
 const AppLogoText = styled.span`
 font-size: 175%;
     text-align: center;
     color: #1a8fff;
     line-height: 1;
+`;
+
+const Main = styled.div`
+    display: block;
+    margin: 0 auto;
+    margin-top: 5em;
+    max-width: 50em;
 `;
 
 class App extends Component {
@@ -87,9 +48,9 @@ class App extends Component {
                         </AppLogoText>
                     </AppLogo>
                 </AppHeader>
-                <main>
+                <Main>
                     <Calendar/>
-                </main>
+                </Main>
 
             </AppWrapper>
         );
